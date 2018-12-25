c579bde52a514c85d85eacdcc1e44d3166c3a5ed
Joseph Borg <josephborg118@gmail.com>
2018-12-25 17:56:41 +0100

    Key retrieval
    1. Added key retrieval for node.

--- node/src/main/java/App.java
+++ node/src/main/java/App.java
@@ -1,11 +1,16 @@
 import com.sun.media.sound.InvalidDataException;
 import command.line.ArgumentParser;
+import model.KeyHolder;
+import security.KeyLoader;
 
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.util.Map;
 
 public class App {
-    private String nodePublicKey;
-    private String nodePrivateKey;
 
     /**
      *
@@ -18,9 +23,15 @@ public class App {
             map = ArgumentParser.convertCommandLineArgs(args);
 
             if (App.isMapValid(map)) {
-                // TODO: Connect with other nodes, if it fails it must reconnect later on but still success....
-                // TODO: This will only handle balance and history request if this node is connected to less than 3 nodes.
-                // TODO: Each node will have its own ledger (csv file) (appendix b)
+                KeyHolder nodeKeys = App.getNodeKeys(map.get("nodename"));
+
+                if (nodeKeys != null) {
+                    // TODO: Connect with other nodes, if it fails it must reconnect later on but still success....
+                    // TODO: This will only handle balance and history request if this node is connected to less than 3 nodes.
+                    // TODO: Each node will have its own ledger (csv file) (appendix b)
+                } else {
+                    throw new InvalidDataException("Invalid node name.");
+                }
             } else {
                 throw new InvalidDataException("Arguments 'nodename' and 'port' are required.");
             }
@@ -33,5 +44,23 @@ public class App {
     private static boolean isMapValid(Map<String, String> map) {
         return map.containsKey("nodename")
                 && map.containsKey("port");
+    }
+
+    private static KeyHolder getNodeKeys(String nodeName) {
+        URL url = App.class.getResource(nodeName.concat(".pfx"));
+
+        try {
+            Path path = Paths.get(url.toURI());
+            String password = nodeName.concat("qwerty");
+
+            PublicKey publicKey = KeyLoader.loadPublicKey(path, nodeName, password);
+            PrivateKey privateKey = KeyLoader.loadPrivateKey(path, nodeName, password, password);
+
+            return new KeyHolder(publicKey, privateKey);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+
+        return null;
     }
 }
